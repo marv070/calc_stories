cd6c8628a2dd7b439fa3678ba82ece0f03659402
Marvin Laucher <marv070@gmail.com>
2017-01-09 13:46:59 -0500

    user story 2 where we multiply number of items times cost.

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require 'sinatra'
 
 
 get'/' do
-  erb :info
+  erb :info, :locals => { :number_of_items => " ", :subtotal => " "}
 end
 
 post '/info' do
-  number_of_items = params[:number_of_items]
-
-erb :info, :locals => { :number_of_items => number_of_items}
+  number_of_items = params[:number_of_items].to_i
+  price_of_item = 100.to_i
+  subtotal = number_of_items * price_of_item
+  erb :info, :locals => { :number_of_items => number_of_items, :subtotal => subtotal}
 end
